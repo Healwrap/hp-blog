34f170cbff893e0d750306bd5b0ab1327776b1ce
pepedd864 <pepedd@qq.com>
2023-05-06 23:58:03 +0800

    加载评论:TODO

--- easybbs-java/easybbs-front-web/src/api/comment.js
+++ easybbs-java/easybbs-front-web/src/api/comment.js
@@ -1,6 +1,22 @@
+import request from '@/utils/request'
+
 const apis = {
   loadComment: '/comment/loadComment',
   postComment: '/comment/postComment',
   doLike: '/comment/doLike',
   changeTopType: '/comment/changeTopType'
 }
+
+const loadComment = params => {
+  return request({
+    url: apis.loadComment,
+    method: 'get',
+    params
+  })
+}
+
+const commentApis = {
+  loadComment
+}
+
+export default commentApis
--- easybbs-java/easybbs-front-web/src/views/forum/ArticleDetail/components/CommentList.vue
+++ easybbs-java/easybbs-front-web/src/views/forum/ArticleDetail/components/CommentList.vue
@@ -4,27 +4,28 @@
       <div class="title">评论<span class="count">0</span></div>
       <div class="tab">
         <span>热榜</span>
-        <el-divider direction="vertical" />
+        <el-divider direction="vertical"/>
         <span>最新</span>
       </div>
     </div>
     <div class="comment-form-panel">
-      <UserAvatar size="default" :user-id="currentUserinfo.userId" />
+      <UserAvatar size="default" :user-id="currentUserinfo.userId"/>
       <div class="comment-form">
         <el-form ref="formDataRef" :model="formData" :rules="rules">
           <!--textarea输入-->
           <el-form-item prop="content">
-            <el-input v-model="formData.content" placeholder="输入评论内容" type="textarea" rows="3" maxlength="800" resize="none" show-word-limit />
+            <el-input v-model="formData.content" placeholder="输入评论内容" type="textarea" rows="3" maxlength="800"
+                      resize="none" show-word-limit/>
           </el-form-item>
         </el-form>
         <div class="comment-form-btn">
           <div v-if="currentUserinfo.userId" class="insert-img">
             <el-upload
-              name="file"
-              :show-file-list="false"
-              accept=".png,.PNG,.jpg,.JPG,.jpeg,.JPEG,.gif,.GIF,.bmp,.BMP"
-              :multiple="false"
-              :http-request="selectImg"
+                name="file"
+                :show-file-list="false"
+                accept=".png,.PNG,.jpg,.JPG,.jpeg,.JPEG,.gif,.GIF,.bmp,.BMP"
+                :multiple="false"
+                :http-request="selectImg"
             >
               <span class="iconfont icon-image"></span>
             </el-upload>
@@ -38,11 +39,12 @@
 </template>
 
 <script setup>
-import { getCurrentInstance, ref, watch } from 'vue'
+import {getCurrentInstance, ref, watch} from 'vue'
+import commentApis from '@/api/comment'
 import UserAvatar from '@/components/Avatar/components/UserAvatar.vue'
 
-const { proxy } = getCurrentInstance()
-defineProps({
+const {proxy} = getCurrentInstance()
+const props = defineProps({
   articleId: {
     type: String
   },
@@ -51,14 +53,18 @@ defineProps({
   }
 })
 const currentUserinfo = ref({})
+// 评论列表
+const commentListInfo = ref({})
+// 排序
+const orderType = ref(0)
 const formData = ref({
   content: ''
 })
 const formDataRef = ref(null)
 const rules = {
   content: [
-    { required: true, message: '请输入评论内容', trigger: 'blur' },
-    { min: 1, max: 800, message: '评论内容长度在 1 到 800 个字符', trigger: 'blur' }
+    {required: true, message: '请输入评论内容', trigger: 'blur'},
+    {min: 1, max: 800, message: '评论内容长度在 1 到 800 个字符', trigger: 'blur'}
   ]
 }
 // 提交评论
@@ -69,15 +75,27 @@ const handleCommentSubmit = () => {
 const selectImg = () => {
   // TODO
 }
-watch(
-  () => proxy.store.getters.userInfo,
-  newVal => {
-    currentUserinfo.value = newVal
-  },
-  {
-    immediate: true,
-    deep: true
+// 加载评论列表
+const loadComment = async () => {
+  const result = await commentApis.loadComment({
+    pageNo: commentListInfo.value.pageNo,
+    articleId: props.articleId,
+    orderType: orderType.value
+  })
+  if (!result) {
+    return
   }
+  commentListInfo.value = result.data
+}
+watch(
+    () => proxy.store.getters.userInfo,
+    newVal => {
+      currentUserinfo.value = newVal
+    },
+    {
+      immediate: true,
+      deep: true
+    }
 )
 </script>
 
