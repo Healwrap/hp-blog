2ff0cac9e18facc65d94a262e12308c5f0f3a7aa
pepedd864 <pepedd@qq.com>
2023-05-18 12:14:18 +0800

    feat: 接口默认的提示内容

--- easybbs-java/easybbs-common/src/main/java/com/easybbs/entity/config/AppConfig.java
+++ easybbs-java/easybbs-common/src/main/java/com/easybbs/entity/config/AppConfig.java
@@ -5,14 +5,25 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
+ * @author pepedd
  * @ClassName AppConfig
  * @Description 应用配置
  * @Date 2023/4/11 18:16
- * @author pepedd
  */
 @Data
 @Component
 public class AppConfig {
+  /**
+   * 项目名称
+   */
+  @Value("${easybbs.name:}")
+  private String name;
+  /**
+   * 项目版本
+   */
+  @Value("${easybbs.version:}")
+  private String version;
+
   @Value("${project.folder:}")
   private String projectFolder;
 }
