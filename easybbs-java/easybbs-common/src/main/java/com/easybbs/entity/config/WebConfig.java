23c8efe2d297b31931b34a52fa1a4b3b6cd3d695
pepedd864 <pepedd@qq.com>
2023-05-19 23:39:23 +0800

    feat: 更新系统缓存

--- easybbs-java/easybbs-common/src/main/java/com/easybbs/entity/config/WebConfig.java
+++ easybbs-java/easybbs-common/src/main/java/com/easybbs/entity/config/WebConfig.java
@@ -6,18 +6,28 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
+ * @author pepedd
  * @ClassName WebConfig
  * @Description web配置
  * @Date 2023/4/11 18:18
- * @author pepedd
  */
 @Data
 @Component
 @EqualsAndHashCode(callSuper = true)
-public class WebConfig extends AppConfig{
+public class WebConfig extends AppConfig {
   @Value("${spring.mail.username:}")
   private String sendUserName;
 
   @Value("${admin.emails:}")
   private String adminEmails;
+
+  @Value("${inner.api.url:}")
+  private String innerApi;
+
+  @Value("${inner.api.appKey:}")
+  private String innerApiAppKey;
+
+  @Value("${inner.api.appSecret:}")
+  private String innerApiAppSecret;
+
 }
---
+++ easybbs-java/easybbs-web/src/main/java/com/easybbs/controller/api/InnerApiController.java
@@ -0,0 +1,50 @@
+package com.easybbs.controller.api;
+
+import com.easybbs.controller.base.ABaseController;
+import com.easybbs.entity.annotation.GlobalIntercepter;
+import com.easybbs.entity.annotation.VerifyParams;
+import com.easybbs.entity.config.WebConfig;
+import com.easybbs.entity.enums.ResponseCodeEnum;
+import com.easybbs.entity.vo.ResponseVO;
+import com.easybbs.exception.BusinessException;
+import com.easybbs.service.SysSettingService;
+import com.easybbs.utils.StringTools;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author pepedd
+ * @ClassName InnerApiController
+ * @Description TODO
+ * @Date 2023/5/19 21:11
+ */
+@RestController
+@RequestMapping("/inner")
+public class InnerApiController extends ABaseController {
+  @Resource
+  private WebConfig webConfig;
+  @Resource
+  private SysSettingService sysSettingService;
+
+  @PostMapping("refreshSysSetting")
+  @GlobalIntercepter(checkParams = true)
+  public ResponseVO refreshSysSetting(@VerifyParams(required = true) String appKey,
+                                      @VerifyParams(required = true) Long timestamp,
+                                      @VerifyParams(required = true) String sign) {
+    if (!webConfig.getInnerApiAppKey().equals(appKey)) {
+      throw new BusinessException(ResponseCodeEnum.CODE_600);
+    }
+    if (System.currentTimeMillis() - timestamp > 1000 * 60 * 5) {
+      throw new BusinessException(ResponseCodeEnum.CODE_600);
+    }
+    String mySign = StringTools.encodeMd5(appKey + timestamp + webConfig.getInnerApiAppSecret());
+    if (!mySign.equals(sign)) {
+      throw new BusinessException(ResponseCodeEnum.CODE_600);
+    }
+    sysSettingService.refreshCache();
+    return new ResponseVO();
+  }
+}
