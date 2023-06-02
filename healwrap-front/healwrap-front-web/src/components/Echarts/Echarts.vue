b500dab90b8d5aca3e3b786f7b2d3d33c8214f15
pepedd864 <pepedd@qq.com>
2023-06-02 09:31:42 +0800

    feat: 加入Echarts图表

---
+++ healwrap-front/healwrap-front-web/src/components/Echarts/Echarts.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="chart">
+    <v-echart class="chart" :option="option" autoresize/>
+  </div>
+</template>
+
+<script>
+// ------基础文件---------
+import VEchart from 'vue-echarts'
+import {use} from 'echarts/core'
+import {CanvasRenderer} from 'echarts/renderers'
+// ------可选文件---------
+import {
+  BarChart,
+  BoxplotChart,
+  CandlestickChart,
+  HeatmapChart,
+  LineChart,
+  RadarChart,
+  SankeyChart,
+  ScatterChart
+} from 'echarts/charts'
+import {
+  GridComponent,
+  LegendComponent,
+  MarkLineComponent,
+  TitleComponent,
+  ToolboxComponent,
+  TooltipComponent,
+  VisualMapComponent
+} from 'echarts/components'
+
+use([
+  CanvasRenderer,
+  BarChart,
+  GridComponent,
+  RadarChart,
+  HeatmapChart,
+  LineChart,
+  ScatterChart,
+  CandlestickChart,
+  BoxplotChart,
+  SankeyChart,
+  TooltipComponent,
+  VisualMapComponent,
+  TitleComponent,
+  LegendComponent,
+  ToolboxComponent,
+  MarkLineComponent
+])
+export default {
+  components: {
+    VEchart
+  },
+  props: {
+    option: {
+      type: Object,
+      default: () => {
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.box {
+  display: flex;
+  justify-content: flex-start;
+}
+</style>
--- healwrap-front/healwrap-front-web/src/views/User/Center/components/UserIntegralRecord.vue
+++ healwrap-front/healwrap-front-web/src/views/User/Center/components/UserIntegralRecord.vue
@@ -24,20 +24,27 @@
           />
         </el-form-item>
       </el-form>
-      <div class="data-title">
-        <div class="record-type">类型</div>
-        <div class="integral">积分</div>
-        <div class="create-type">时间</div>
-      </div>
-      <data-list :loading="loading" :data-source="recordInfo" @load-data="loadRecord">
-        <template #default="{ data }">
-          <div class="data-item">
-            <div class="record-type">{{ data.operTypeName }}</div>
-            <div class="integral" :style="{ color: data.integral > 0 ? 'green' : 'red' }">{{ data.integral }}</div>
-            <div class="create-type">{{ data.createTime }}</div>
+      <el-tabs>
+        <el-tab-pane label="图表">
+          <echarts class="echarts" :option="option" />
+        </el-tab-pane>
+        <el-tab-pane label="列表">
+          <div class="data-title">
+            <div class="record-type">类型</div>
+            <div class="integral">积分</div>
+            <div class="create-type">时间</div>
           </div>
-        </template>
-      </data-list>
+          <data-list :loading="loading" :data-source="recordInfo" @load-data="loadRecord">
+            <template #default="{ data }">
+              <div class="data-item">
+                <div class="record-type">{{ data.operTypeName }}</div>
+                <div class="integral" :style="{ color: data.integral > 0 ? 'green' : 'red' }">{{ data.integral }}</div>
+                <div class="create-type">{{ data.createTime }}</div>
+              </div>
+            </template>
+          </data-list>
+        </el-tab-pane>
+      </el-tabs>
     </Dialog>
   </div>
 </template>
@@ -46,9 +53,41 @@
 // 弹窗配置
 import { getCurrentInstance, reactive, ref } from 'vue'
 import DataList from '@/components/DataList/DataList.vue'
+import Echarts from '@/components/Echarts/Echarts.vue'
 
 const { proxy } = getCurrentInstance()
 const loading = ref(true)
+const option = reactive({
+  title: {
+    text: '积分变化',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'axis'
+  },
+  xAxis: {
+    type: 'category',
+    data: []
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      name: '积分变化',
+      type: 'line',
+      data: [],
+      markLine: {
+        silent: true,
+        data: [
+          {
+            yAxis: 0
+          }
+        ]
+      }
+    }
+  ]
+})
 const recordInfo = ref({})
 const props = defineProps({})
 const formDataRef = ref(null)
@@ -95,6 +134,8 @@ const loadRecord = async () => {
     return
   }
   recordInfo.value = result.data
+  option.xAxis.data = result.data.list.map(item => item.createTime.substr(0, 10))
+  option.series[0].data = result.data.list.map(item => item.integral)
   loading.value = false
 }
 defineExpose({
@@ -121,6 +162,11 @@ defineExpose({
     .create-type {
       width: 30%;
     }
+  }
+
+  .echarts {
+    width: 100%;
+    height: 300px;
   }
 
   .data-item {
